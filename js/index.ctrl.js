8758b2ca12da79d409d98f2b5449900aa2685c88
Karen Snyder <karen.d.snyder@advantagesolutions.net>
2019-09-12 15:22:54 -0700

    first commit

---
+++ js/index.ctrl.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    var map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([-97.6114, 38.8403]),
+            zoom: 5
+        })
+    });
+
+});
