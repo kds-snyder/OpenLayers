5e1622771a6f1900814f0f836f2df3d34a9cdbb4
Karen Snyder <karen.d.snyder@advantagesolutions.net>
2019-09-13 09:47:02 -0700

    converted OpenLayers to ol, but still failing at line 23 with error ol.Feature.Vector is not a constructor

--- js/index.ctrl.js
+++ js/index.ctrl.js
@@ -1,36 +1,44 @@
-$(document).ready(function () {
+$(document).ready(function () {    
 
-    var map = new OpenLayers.Map("mapdiv");
-    map.addLayer(new OpenLayers.Layer.OSM());
+    var map = new ol.Map({
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        target: 'mapdiv',
+        view: new ol.View({
+            center: ol.proj.fromLonLat([-0.1279688, 51.5077286]),
+            maxZoom: 18,
+            zoom: 12
+        })
+    });
 
-    epsg4326 = new OpenLayers.Projection("EPSG:4326"); //WGS 1984 projection
-    projectTo = map.getProjectionObject(); //The map projection (Spherical Mercator)
+    //var epsg4326 = new ol.Projection("EPSG:4326"); //WGS 1984 projection
+    //var projectTo = map.getProjectionObject(); //The map projection (Spherical Mercator)
 
-    var lonLat = new OpenLayers.LonLat(-0.1279688, 51.5077286).transform(epsg4326, projectTo);
-
-
-    var zoom = 14;
-    map.setCenter(lonLat, zoom);
-
-    var vectorLayer = new OpenLayers.Layer.Vector("Overlay");
+    var vectorLayer = new ol.layer.Vector("Overlay");
 
     // Define markers as "features" of the vector layer:
-    var feature = new OpenLayers.Feature.Vector(
-        new OpenLayers.Geometry.Point(-0.1279688, 51.5077286).transform(epsg4326, projectTo),
+    var feature = new ol.Feature.Vector(
+        //new ol.geom.Point(-0.1279688, 51.5077286).transform(epsg4326, projectTo),
+        new ol.geom.Point(-0.1279688, 51.5077286),
         { description: 'This is the value of<br>the description attribute' },
         { externalGraphic: 'images/marker-editor.svg', graphicHeight: 25, graphicWidth: 21, graphicXOffset: -12, graphicYOffset: -25 }
     );
     vectorLayer.addFeatures(feature);
 
-    var feature = new OpenLayers.Feature.Vector(
-        new OpenLayers.Geometry.Point(-0.1244324, 51.5006728).transform(epsg4326, projectTo),
+    var feature = new ol.Feature.Vector(
+        //new ol.geom.Point(-0.1244324, 51.5006728).transform(epsg4326, projectTo),
+        new ol.geom.Point(-0.1244324, 51.5006728),
         { description: 'Big Ben' },
         { externalGraphic: 'images/marker-editor.svg', graphicHeight: 25, graphicWidth: 21, graphicXOffset: -12, graphicYOffset: -25 }
     );
     vectorLayer.addFeatures(feature);
 
-    var feature = new OpenLayers.Feature.Vector(
-        new OpenLayers.Geometry.Point(-0.119623, 51.503308).transform(epsg4326, projectTo),
+    var feature = new ol.Feature.Vector(
+        //new ol.geom.Point(-0.119623, 51.503308).transform(epsg4326, projectTo),
+        new ol.geom.Point(-0.119623, 51.503308),
         { description: 'London Eye' },
         { externalGraphic: 'images/marker-editor.svg', graphicHeight: 25, graphicWidth: 21, graphicXOffset: -12, graphicYOffset: -25 }
     );
@@ -42,11 +50,11 @@ $(document).ready(function () {
 
     //Add a selector control to the vectorLayer with popup functions
     var controls = {
-        selector: new OpenLayers.Control.SelectFeature(vectorLayer, { onSelect: createPopup, onUnselect: destroyPopup })
+        selector: new ol.Control.SelectFeature(vectorLayer, { onSelect: createPopup, onUnselect: destroyPopup })
     };
 
     function createPopup(feature) {
-        feature.popup = new OpenLayers.Popup.FramedCloud("pop",
+        feature.popup = new ol.Popup.FramedCloud("pop",
             feature.geometry.getBounds().getCenterLonLat(),
             null,
             '<div class="markerContent">' + feature.attributes.description + '</div>',
