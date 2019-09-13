c11b6a03b673f11458cf3abf469a65f2f6a5b716
Karen Snyder <karen.d.snyder@advantagesolutions.net>
2019-09-12 17:03:20 -0700

    Get set of features using solution from https://harrywood.co.uk/maps/examples/openlayers/marker-popups.view.html

--- js/index.ctrl.js
+++ js/index.ctrl.js
@@ -1,16 +1,69 @@
 $(document).ready(function () {
 
-    var map = new ol.Map({
-        target: 'map',
-        layers: [
-            new ol.layer.Tile({
-                source: new ol.source.OSM()
-            })
-        ],
-        view: new ol.View({
-            center: ol.proj.fromLonLat([-97.6114, 38.8403]),
-            zoom: 5
-        })
-    });
+    var map = new OpenLayers.Map("mapdiv");
+    map.addLayer(new OpenLayers.Layer.OSM());
+
+    epsg4326 = new OpenLayers.Projection("EPSG:4326"); //WGS 1984 projection
+    projectTo = map.getProjectionObject(); //The map projection (Spherical Mercator)
+
+    var lonLat = new OpenLayers.LonLat(-0.1279688, 51.5077286).transform(epsg4326, projectTo);
+
+
+    var zoom = 14;
+    map.setCenter(lonLat, zoom);
+
+    var vectorLayer = new OpenLayers.Layer.Vector("Overlay");
+
+    // Define markers as "features" of the vector layer:
+    var feature = new OpenLayers.Feature.Vector(
+        new OpenLayers.Geometry.Point(-0.1279688, 51.5077286).transform(epsg4326, projectTo),
+        { description: 'This is the value of<br>the description attribute' },
+        { externalGraphic: 'images/marker-editor.svg', graphicHeight: 25, graphicWidth: 21, graphicXOffset: -12, graphicYOffset: -25 }
+    );
+    vectorLayer.addFeatures(feature);
+
+    var feature = new OpenLayers.Feature.Vector(
+        new OpenLayers.Geometry.Point(-0.1244324, 51.5006728).transform(epsg4326, projectTo),
+        { description: 'Big Ben' },
+        { externalGraphic: 'images/marker-editor.svg', graphicHeight: 25, graphicWidth: 21, graphicXOffset: -12, graphicYOffset: -25 }
+    );
+    vectorLayer.addFeatures(feature);
+
+    var feature = new OpenLayers.Feature.Vector(
+        new OpenLayers.Geometry.Point(-0.119623, 51.503308).transform(epsg4326, projectTo),
+        { description: 'London Eye' },
+        { externalGraphic: 'images/marker-editor.svg', graphicHeight: 25, graphicWidth: 21, graphicXOffset: -12, graphicYOffset: -25 }
+    );
+    vectorLayer.addFeatures(feature);
+
+
+    map.addLayer(vectorLayer);
+
+
+    //Add a selector control to the vectorLayer with popup functions
+    var controls = {
+        selector: new OpenLayers.Control.SelectFeature(vectorLayer, { onSelect: createPopup, onUnselect: destroyPopup })
+    };
+
+    function createPopup(feature) {
+        feature.popup = new OpenLayers.Popup.FramedCloud("pop",
+            feature.geometry.getBounds().getCenterLonLat(),
+            null,
+            '<div class="markerContent">' + feature.attributes.description + '</div>',
+            null,
+            true,
+            function () { controls['selector'].unselectAll(); }
+        );
+        //feature.popup.closeOnMove = true;
+        map.addPopup(feature.popup);
+    }
+
+    function destroyPopup(feature) {
+        feature.popup.destroy();
+        feature.popup = null;
+    }
+
+    map.addControl(controls['selector']);
+    controls['selector'].activate();
 
 });
