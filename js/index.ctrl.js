4c99cd9a876e963549b2e717650390212020e98d
Karen Snyder <karen.d.snyder@advantagesolutions.net>
2019-09-14 13:08:50 -0700

    aded images/marker-editor.png

--- js/index.ctrl.js
+++ js/index.ctrl.js
@@ -8,70 +8,77 @@ $(document).ready(function () {
         ],
         target: 'mapdiv',
         view: new ol.View({
-            center: ol.proj.fromLonLat([-0.1279688, 51.5077286]),
+            center: ol.proj.fromLonLat([-97.6114, 38.8403]),
             maxZoom: 18,
-            zoom: 12
+            zoom: 5
         })
     });
 
-    //var epsg4326 = new ol.Projection("EPSG:4326"); //WGS 1984 projection
-    //var projectTo = map.getProjectionObject(); //The map projection (Spherical Mercator)
-
-    var vectorLayer = new ol.layer.Vector("Overlay");
-
     // Define markers as "features" of the vector layer:
-    var feature = new ol.Feature.Vector(
-        //new ol.geom.Point(-0.1279688, 51.5077286).transform(epsg4326, projectTo),
-        new ol.geom.Point(-0.1279688, 51.5077286),
-        { description: 'This is the value of<br>the description attribute' },
-        { externalGraphic: 'images/marker-editor.svg', graphicHeight: 25, graphicWidth: 21, graphicXOffset: -12, graphicYOffset: -25 }
-    );
-    vectorLayer.addFeatures(feature);
+    var features = [
+        new ol.Feature(
+            new ol.geom.Point(ol.proj.fromLonLat([-117.8491357, 33.68315])),
+            { description: 'Irvine' }
+        ),
+        new ol.Feature(
+            new ol.geom.Point(ol.proj.fromLonLat([-84.058846, 33.963502])),
+            { description: 'Lawrenceville' }
+        ),
+        new ol.Feature(
+            new ol.geom.Point(ol.proj.fromLonLat([-117.209298, 32.880391])),
+            { description: 'San Diego' },
+        ),
 
-    var feature = new ol.Feature.Vector(
-        //new ol.geom.Point(-0.1244324, 51.5006728).transform(epsg4326, projectTo),
-        new ol.geom.Point(-0.1244324, 51.5006728),
-        { description: 'Big Ben' },
-        { externalGraphic: 'images/marker-editor.svg', graphicHeight: 25, graphicWidth: 21, graphicXOffset: -12, graphicYOffset: -25 }
-    );
-    vectorLayer.addFeatures(feature);
+        new ol.Feature(
+            new ol.geom.Point(ol.proj.fromLonLat([-82.351164, 27.978778])),
+            { description: 'Tampa' },
+        )
+    ];
 
-    var feature = new ol.Feature.Vector(
-        //new ol.geom.Point(-0.119623, 51.503308).transform(epsg4326, projectTo),
-        new ol.geom.Point(-0.119623, 51.503308),
-        { description: 'London Eye' },
-        { externalGraphic: 'images/marker-editor.svg', graphicHeight: 25, graphicWidth: 21, graphicXOffset: -12, graphicYOffset: -25 }
-    );
-    vectorLayer.addFeatures(feature);
+    function iconStyleFunc() {
+        iconStyle = [new ol.style.Style({
+            image: new ol.style.Icon(({
+                scale: 0.25,
+                src: 'images/marker-editor.png'
+            }))
+        })];
+        return iconStyle;
+    }
 
+    var vectorLayer = new ol.layer.Vector({
+        source: new ol.source.Vector({
+            features: features
+        }),
+        style: iconStyleFunc()
+    });
 
     map.addLayer(vectorLayer);
 
 
     //Add a selector control to the vectorLayer with popup functions
-    var controls = {
-        selector: new ol.Control.SelectFeature(vectorLayer, { onSelect: createPopup, onUnselect: destroyPopup })
-    };
+    //var controls = {
+    //    selector: new ol.Control.SelectFeature(vectorLayer, { onSelect: createPopup, onUnselect: destroyPopup })
+    //};
 
-    function createPopup(feature) {
-        feature.popup = new ol.Popup.FramedCloud("pop",
-            feature.geometry.getBounds().getCenterLonLat(),
-            null,
-            '<div class="markerContent">' + feature.attributes.description + '</div>',
-            null,
-            true,
-            function () { controls['selector'].unselectAll(); }
-        );
-        //feature.popup.closeOnMove = true;
-        map.addPopup(feature.popup);
-    }
+    //function createPopup(feature) {
+    //    feature.popup = new ol.Popup.FramedCloud("pop",
+    //        feature.geometry.getBounds().getCenterLonLat(),
+    //        null,
+    //        '<div class="markerContent">' + feature.attributes.description + '</div>',
+    //        null,
+    //        true,
+    //        function () { controls['selector'].unselectAll(); }
+    //    );
+    //    //feature.popup.closeOnMove = true;
+    //    map.addPopup(feature.popup);
+    //}
 
-    function destroyPopup(feature) {
-        feature.popup.destroy();
-        feature.popup = null;
-    }
+    //function destroyPopup(feature) {
+    //    feature.popup.destroy();
+    //    feature.popup = null;
+    //}
 
-    map.addControl(controls['selector']);
-    controls['selector'].activate();
+    //map.addControl(controls['selector']);
+    //controls['selector'].activate();
 
 });
