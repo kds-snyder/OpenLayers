2d88f27191a0582a08274f450da19344cd4e2736
Karen Snyder <karen.d.snyder@advantagesolutions.net>
2019-09-23 11:15:49 -0700

    Display popup that does not have feature content

--- js/index.ctrl.js
+++ js/index.ctrl.js
@@ -52,6 +52,41 @@ $(document).ready(function () {
     });
     map.addLayer(vectorLayer);
 
+    // Set popup
+    var container = document.getElementById('popup');
+    var content = document.getElementById('popup-content');
+    var closer = document.getElementById('popup-closer');
+
+    var overlay = new ol.Overlay({
+        element: container,
+        autoPan: true,
+        autoPanAnimation: {
+            duration: 250
+        }
+    });
+    map.addOverlay(overlay);
+
+    closer.onclick = function () {
+        overlay.setPosition(undefined);
+        closer.blur();
+        return false;
+    };
+
+    map.on('singleclick', function (event) {
+        if (map.hasFeatureAtPixel(event.pixel) === true) {
+            console.log('event clicked:');
+            console.log(event);
+            var coordinate = event.coordinate;
+
+            content.innerHTML = '<b>Hello world!</b><br />I am a popup.';
+            overlay.setPosition(coordinate);
+        } else {
+            overlay.setPosition(undefined);
+            closer.blur();
+        }
+    });
+
+
     //Add a selector control to the vectorLayer with popup functions
     //var controls = {
     //    selector: new ol.Control.SelectFeature(vectorLayer, { onSelect: createPopup, onUnselect: destroyPopup })
